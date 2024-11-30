fd07391f0655d1971046e844fe97db9b0779278a
Shaggy030601 <tovaruriel2001@gmail.com>
2024-11-30 07:25:46 -0600

    mi desarrollo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,44 @@
-export default function Pages(){
-    return <p>este dato pertenece a la pege de dashboard</p>
+import { fetchLatestInvoices, fetchRevenue,fetchCardData } from "../lib/data"
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from "react";
+import { RevenueChartSkeleton } from "../ui/skeletons";
+
+export default async function Pages(){
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+//3
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl py-10 flex justify-center items-center `}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" /> 
+        <Card title="Pending" value={totalPendingInvoices} type="pending" /> 
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> 
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> 
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton></RevenueChartSkeleton>}>
+         <RevenueChart />
+         </Suspense>
+        {<Suspense fallback={<RevenueChartSkeleton></RevenueChartSkeleton>}>
+          <LatestInvoices  /> 
+          </Suspense>
+          } 
+      </div>
+    </main>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import { monserrat } from './ui/fonts';
 import './ui/global.css';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
@@ -8,11 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={`${monserrat.className} antialiased`}>
         {children}
         <footer className='py-10 flex justify-center items-center'>
-          ESTE DESARROLLO PERTENECE A MI PRACTICA DE EXAMEN
+        <p>2024 Derechos Reservados por Ing. Alfredo Tovar - Desarrollador Web.</p>
         </footer>
         </body>
     </html>
